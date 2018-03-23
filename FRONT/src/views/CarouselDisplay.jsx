8d3a092346eefe3febe0f7ed0739d94cf00b9525
juanzetta <juan.panzetta@mercadolibre.com>
2018-03-23 11:44:11 -0300

    api
    api8

--- FRONT/src/views/CarouselDisplay.jsx
+++ FRONT/src/views/CarouselDisplay.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import api from "../api"
 import CarouselEntity from './CarouselEntity'
 import {
   Carousel,
@@ -7,60 +8,25 @@ import {
   CarouselIndicators
 } from 'reactstrap';
 
-const items = [
-  {
-    thumbnail: "https://http2.mlstatic.com/celulares-motorola-D_Q_NP_781526-MLA26962080389_032018-X.webp",
-    price: 2.499,
-    tittle: "Motorola Moto C 4g 8gb Nuevo Libre Gtía 1 Año Techcel Motorola Moto C 4g 8gb Nuevo Libre Gtía 1 Año Techcel Motorola Moto C 4g 8gb Nuevo Libre Gtía 1 Año Techcel Motorola Moto C 4g 8gb Nuevo Libre Gtía 1 Año Techcel",
-    location: "Cordoba",
-    condition: "Nuevo"
-  },
-  {
-    thumbnail: "https://http2.mlstatic.com/camas-cuchetas-D_Q_NP_970172-MLA26064700803_092017-X.webp",
-    price: 5.309,
-    tittle: "Cama Cucheta Triple Superpuesta Mdf Laqueada Blanco Infantil",
-    location: "Mendoza",
-    condition: "Nuevo"
-  },
-  {
-    thumbnail: "https://http2.mlstatic.com/zapatillas-D_Q_NP_887999-MLA26260623016_102017-X.webp",
-    price: 849,
-    tittle: "Zapatillas Volcom Hombre Ninety One",
-    location: "Buenos Aires",
-    condition: "Usado"
-  },
-  {
-    thumbnail: "https://http2.mlstatic.com/celulares-motorola-D_Q_NP_781526-MLA26962080389_032018-X.webp",
-    price: 2.499,
-    tittle: "Motorola Moto C 4g 8gb Nuevo Libre Gtía 1 Año Techcel Motorola Moto C 4g 8gb Nuevo Libre Gtía 1 Año Techcel Motorola Moto C 4g 8gb Nuevo Libre Gtía 1 Año Techcel Motorola Moto C 4g 8gb Nuevo Libre Gtía 1 Año Techcel",
-    location: "Cordoba",
-    condition: "Nuevo"
-  },
-  {
-    thumbnail: "https://http2.mlstatic.com/camas-cuchetas-D_Q_NP_970172-MLA26064700803_092017-X.webp",
-    price: 5.309,
-    tittle: "Cama Cucheta Triple Superpuesta Mdf Laqueada Blanco Infantil",
-    location: "Mendoza",
-    condition: "Nuevo"
-  },
-  {
-    thumbnail: "https://http2.mlstatic.com/zapatillas-D_Q_NP_887999-MLA26260623016_102017-X.webp",
-    price: 849,
-    tittle: "Zapatillas Volcom Hombre Ninety One",
-    location: "Buenos Aires",
-    condition: "Usado"
-  }
-];
-
 class CarouselDisplay extends Component {
   constructor(props) {
     super(props);
-    this.state = { activeIndex: 0 };
+    this.state = {
+      activeIndex: 0,
+      items: []
+    };
     this.next = this.next.bind(this);
     this.previous = this.previous.bind(this);
     this.goToIndex = this.goToIndex.bind(this);
     this.onExiting = this.onExiting.bind(this);
     this.onExited = this.onExited.bind(this);
+  }
+
+  componentWillMount() {
+    api.getItems()
+      .then(res => {
+        this.setState({ items: res.data })
+      });
   }
 
   onExiting() {
@@ -73,13 +39,13 @@ class CarouselDisplay extends Component {
 
   next() {
     if (this.animating) return;
-    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    const nextIndex = this.state.activeIndex === this.state.items.length - 1 ? 0 : this.state.activeIndex + 1;
     this.setState({ activeIndex: nextIndex });
   }
 
   previous() {
     if (this.animating) return;
-    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    const nextIndex = this.state.activeIndex === 0 ? this.state.items.length - 1 : this.state.activeIndex - 1;
     this.setState({ activeIndex: nextIndex });
   }
 
@@ -91,15 +57,15 @@ class CarouselDisplay extends Component {
   render() {
     const { activeIndex } = this.state;
 
-    const slides = items.map((item) => {
+    const slides = this.state.items.map((item, i) => {
       return (
         <CarouselItem
           style={{ "padding-left": "70px" }}
           onExiting={this.onExiting}
           onExited={this.onExited}
-          key={item.src}
+          key={i}
         >
-          <CarouselEntity obj={item} reference={"https://google.com.ar/"} thumbnail={item.thumbnail} price={item.price} tittle={item.tittle} location={item.location} condition={item.condition} />
+          <CarouselEntity obj={item} key={i} reference={"https://google.com.ar/"} thumbnail={item.thumbnail} price={item.price} title={item.title} location={item.state_name} condition={item.item_condition} />
           { /* <CarouselCaption captionText={item.altText} captionHeader={item.caption} style={{ position: "absolute", right: "0px", top: "10%", margin: "0% 10%", marginLeft: "30%" }} />*/}
         </CarouselItem>
       );
@@ -112,7 +78,7 @@ class CarouselDisplay extends Component {
         next={this.next}
         previous={this.previous}
       >
-        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+        <CarouselIndicators items={this.state.items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
         {slides}
         <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
         <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
