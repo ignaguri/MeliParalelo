535f703e610c18cd18f52e6812a3d957dd03bbf1
juanzetta <juan.panzetta@mercadolibre.com>
2018-03-23 09:20:01 -0300

    api
    api

---
+++ FRONT/src/api/index.js
@@ -0,0 +1,115 @@
+const axios = require('axios');
+//axios.defaults.headers.post['Content-Type'] = 'application/json';
+const API = "http://localhost:8080/";
+
+
+export default {
+
+
+    //login
+        //401 unathoraized devuelve error: "usuario inexistente"/"contraseña incorrecta"
+        //500 otro error
+        //200 ok    //devuelve username, name lastname
+    postLogin(user, password){
+        return axios.post(API+'user/login', { username: user, password: password })
+            .then(function(response){
+                console.log(response);
+                return response
+                
+            })
+            .catch(err => {
+                console.error(err);
+                if (err.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    // console.log(error.response.data);
+                    // console.log(error.response.status);
+                    // console.log(error.response.headers);
+                    return err.response.data
+                }
+                return false
+            })
+    },
+
+
+    //Crear Usuario
+    postSingin(user, password, name, lastname, birthdate, email){
+        return axios.post('/singin', { username: user, password: password, name: name, lastname: lastname, birthdate: birthdate, email: email })
+            .then(function(response){
+                return response
+            })
+            .catch(err => {
+                console.error(err);
+                return false
+            })
+    },
+    
+    getCategories() {
+        return axios.get(API+'categories')
+            .then(r => {
+                console.log(r);
+                return r
+            })
+            .catch(err => {
+                console.error(err);
+                return false
+            })
+    },
+
+    postPreferences(categories){
+        return axios.post('/preferences', { cateegories: categories})
+            .then(function(response){
+                return response
+            })
+            .catch(err => {
+                console.error(err);
+                return false
+            })
+    },
+
+
+
+    //Pagina Principal
+
+    getItems() {
+        return axios.get(API + 'items')
+            .then(r => {
+                console.log(r);
+                return r
+            })
+            .catch(err => {
+                console.error(err);
+                return false
+            })
+    },
+
+    //cargar combo filtros
+        //arreglo strings 
+    getLocations() {
+        return axios.get(API + 'localidades')
+            .then(r => {
+                console.log(r);
+                return r
+            })
+            .catch(err => {
+                console.error(err);
+                return false
+            })
+    },
+
+    //get categorias ya esta arriba
+
+
+
+    
+
+
+
+
+
+
+
+    
+    
+}
+
